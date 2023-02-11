5cd767334109b4e1decfa2c0b4b46e5b34200078
Barnabas27 <bnyambura032@gmail.com>
2023-02-12 01:36:22 +0300

    add basic set-up

---
+++ 0x00-Webpack/task_0/webpack.config.js
@@ -0,0 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+};
