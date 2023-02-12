2c8ccada06c050e9e9b0da083ad77b78bcd8a7c3
Barnabas27 <bnyambura032@gmail.com>
2023-02-12 04:02:18 +0300

    add entry point

--- 0x00-Webpack/task_0/webpack.config.js
+++ 0x00-Webpack/task_0/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, './src/index.js'),
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
   },
   output: {
     filename: '[name].js',
