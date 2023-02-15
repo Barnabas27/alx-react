41036f26946fad6d8cb8bccb9be88d2243401d22
Barnabas27 <bnyambura032@gmail.com>
2023-02-15 06:05:25 +0300

    add devdepencies

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,16 +1,10 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require('path');
+const path = require("path");
 module.exports = {
-    entry: {
-        bundle:path.resolve(__dirname, "./js/dashboard_main.js")
-    },
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "public")
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "learning webpack"
-        }),
-    ],
+  entry: {
+    bundle: path.resolve(__dirname, "./js/dashboard_main.js"),
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "public"),
+  },
 };
