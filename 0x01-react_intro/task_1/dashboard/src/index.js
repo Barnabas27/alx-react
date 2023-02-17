fa6b9249534f28f7faf95b6d966c19a8620b1741
Barnabas27 <bnyambura032@gmail.com>
2023-02-17 10:39:13 +0300

    add change of the root API

--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -4,21 +4,19 @@ import App from './App';
 import Notifications from './Notifications';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-ReactDOM.render(
-	<React.StrictMode>
-		<Notifications />
-	</React.StrictMode>,
-	document.getElementById('root-notifications')
+const rootNotifications = ReactDOM.createRoot(document.getElementById("root-notifications"));
+rootNotifications.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
 );
-
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
